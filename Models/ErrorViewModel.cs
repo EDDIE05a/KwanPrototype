8e9f34c97842545fc4c81c547dc971b70090d73f
Eddie <eddarenas@soy.sena.edu.co>
2024-11-20 18:50:43 -0500

    Primer commit subiendo proyecto de visual studio code

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace KwanPrototypeMVC.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
